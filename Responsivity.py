a1c8fc04ab80c09c9c5e519ef6d4196674ca65eb
L.C. Huirne <l.huirne@outlook.com>
2024-10-17 16:21:14 +0200

    Add Responsivity.py

---
+++ Responsivity.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load the data
+data_228 = pd.read_csv('NewFile0.csv')
+data_200 = pd.read_csv('NewFile1.csv')
+data_184 = pd.read_csv('NewFile2.csv')
+data_160 = pd.read_csv('NewFile3.csv')
+data_144 = pd.read_csv('NewFile4.csv')
+data_120 = pd.read_csv('NewFile5.csv')
+data_100 = pd.read_csv('NewFile6.csv')
+data_84 = pd.read_csv('NewFile7.csv')
+data_64 = pd.read_csv('NewFile8.csv')
+data_40 = pd.read_csv('NewFile9.csv')
