8db8a6a0be9b3e9d9eb8d755d115d761b21d2acb
marnick1268 <M.A.Stoll@student.tudelft.nl>
2024-10-19 14:01:55 +0200

    l

--- numderivative.py
+++ numderivative.py
@@ -10,19 +10,42 @@ def f(x):
 def fp(x):
     return np.cos(x)
 
+
+A=np.array([[1,1,1,1,1],
+           [-2,-1,0,2,1],
+           [4,1,0,1,4],
+           [-8,-1,0,1,8],
+           [16,1,0,1,16]])
+b=np.array([0,1,0,0,0])
+
+x=np.linalg.solve(A,b)
+
+#for i in range(21+1):
+#    print(i)
+#    print(x*i)
+
+#print(sum(x))
+#for i in range(5):
+#    print(np.transpose(A[i]))
+#
+#    print(np.matmul(A[i],np.transpose(x)))
+
+print(x)
+print(f'1/12*{x*21}')
+
 xlst=np.linspace(0,20,200)
 
 xplst=fp(xlst)
 xptlst=np.array([])
 for i in range(200):
-    print(i)
+    #print(i)
     #is only valid when ui-2 etc exist
     if i<2 or i>197:
         xptlst=np.append(xptlst,fp(xlst[i]))
     else:
-        xptlst=np.append(xptlst,fifthordernumderivative(xlst,i,1/10))
+        xptlst=np.append(xptlst,fifthordernumderivative(xlst,i,(xlst[1]-xlst[0])))
 err=xptlst-xplst
-print(sum(err)/200)
+print(f'error={abs(sum(err)/len(xlst))}')
 plt.figure()
 plt.plot(xlst,xplst,'-g')
 plt.plot(xlst,xptlst,'-b')
