2d64eda1a59034b71b1684b32005642728fee65f
L.C. Huirne <l.huirne@outlook.com>
2024-10-21 00:53:11 +0200

    added peak detection

--- Responsivity.py
+++ Responsivity.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+from cornerdetection import cornerdetection_inputsignal, max_turnaround, to_the_sides, golay_filter_variable_window_size, cornerdetection_outputsignal
+from numderivative import nthorderfirstdegreenumderivative
+from scipy.signal import savgol_filter
 
 # Load the data
 data_228 = pd.read_csv('Data/NewFile0.csv')
@@ -18,14 +21,30 @@ data_40 = pd.read_csv('Data/NewFile9.csv')
 data_array = [data_228, data_200, data_184, data_160, data_144, data_120, data_100, data_84, data_64, data_40]
 
 
-
+figax = []
 # Extract the data
-for df in [data_array[0]]:
-    fig, ax = plt.subplots()
-    ax.scatter(df.iloc[1:,0].to_numpy().astype(float), df.iloc[1:,1].to_numpy().astype(float), s=1)
-    ax.scatter(df.iloc[1:,0].to_numpy().astype(float), df.iloc[1:,2].to_numpy().astype(float), s=1)
+for counter, df in enumerate([data_array[5]]):
+
+    time = df.iloc[1:,0].to_numpy().astype(float)
+    input_signal = df.iloc[1:,1].to_numpy().astype(float)
+    output_signal = df.iloc[1:,2].to_numpy().astype(float)
+
+    input_signal_peaks = cornerdetection_inputsignal(input_signal, time, True)
+    output_signal_peaks = cornerdetection_outputsignal(output_signal, time, True)
+    input_signal_throughs = cornerdetection_inputsignal(input_signal, time, False)
+    output_signal_throughs = cornerdetection_outputsignal(output_signal, time, False)
 
 
-    ax.grid()
+    figax.append(plt.subplots())
+    figax[counter][1].scatter(time,input_signal , s=1)
+    #figax[counter][1].vlines(x=input_signal_peaks, color='red', ymin=min(input_signal), ymax=max(input_signal))
+    #figax[counter][1].vlines(x=input_signal_throughs, color='green', ymin=min(input_signal), ymax=max(input_signal))
+    figax[counter][1].scatter(time, output_signal, s=1)
+    figax[counter][1].scatter(time, savgol_filter(input_signal, 100, 4, deriv=0, delta=(time[1]-time[0])), s=1)
+    figax[counter][1].plot(time, golay_filter_variable_window_size(output_signal, 4, time, deriv=0), linewidth=1)
+    #figax[counter][1].vlines(x=output_signal_peaks, color='red', ymin=min(output_signal), ymax=max(output_signal))
+    #figax[counter][1].vlines(x=output_signal_throughs, color='green', ymin=min(output_signal), ymax=max(output_signal))
+
+    #figax[counter][1].grid()
     plt.show()
 
---
+++ cornerdetection.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+from numderivative import nthorderfirstdegreenumderivative, nthorderjthdegreenumderivative
+from scipy.signal import savgol_filter
+
+
+
+def cornerdetection_inputsignal(data_array, time_array, peak_or_through):
+    """
+    Detect the corner of the input signal
+    """
+    corner = []
+    y = savgol_filter(data_array, 100, 4, deriv=1, delta=time_array[1]-time_array[0]) #nthorderfirstdegreenumderivative(50, data_array, time_array[1] - time_array[0])
+    for [counter, derivative], time in zip(enumerate(y), time_array):
+        if peak_or_through == True:
+            if counter < len(y)-1:
+                if (derivative >= 0) & (y[counter+1] <= 0):
+                    corner.append(time)
+        else:
+            if counter < len(y)-1:
+                if (derivative <= 0) & (y[counter+1] >= 0):
+                    corner.append(time)
+    return corner
+
+def golay_filter_variable_window_size(data_array, polynomial_order, time_array, deriv=0):
+    local_window_size = np.array(to_the_sides(max_turnaround(
+        savgol_filter(abs(savgol_filter(data_array, 10, polynomial_order, deriv=2, delta=(time_array[1] - time_array[0]))), 100,
+                      polynomial_order)) / 2000000))
+    for xi, xf in enumerate(local_window_size):
+        if xf <= polynomial_order + 1:
+            local_window_size[xi] = polynomial_order + 1
+    y = np.zeros(len(data_array))
+    for i in range(len(y)):
+        y[i] = savgol_filter(data_array, int(local_window_size[i]), polynomial_order, deriv=deriv, delta=time_array[1] - time_array[0])[
+            i]  # nthorderfirstdegreenumderivative(50, data_array, time_array[1] - time_array[0])
+    return y
+
+def cornerdetection_outputsignal(data_array, time_array,peak_or_through):
+    """
+    Detect the corner of the output signal
+    """
+    corner = []
+    local_window_size = np.array(to_the_sides(max_turnaround(savgol_filter(abs(savgol_filter(data_array, 10, 4, deriv=2, delta=(time_array[1] - time_array[0]))),100, 4 ))/2000000))
+    y = golay_filter_variable_window_size(data_array, 4, time_array, deriv=1)
+
+    for [counter, derivative], time in zip(enumerate(y), time_array):
+        if peak_or_through == True:
+            if counter < len(y)-1:
+                if (derivative >= 0) & (y[counter+1] <= 0):
+                    corner.append(time)
+        else:
+            if counter < len(y)-1:
+                if (derivative <= 0) & (y[counter+1] >= 0):
+                    corner.append(time)
+    return corner
+
+def max_turnaround(x):
+    return max(x)*np.ones(len(x)) - x
+
+def to_the_sides(x):
+    for xi, xf in enumerate(x):
+        if (xf <= 0) & (xi > 0):
+            xf = x[xi-1]
+        elif (xf <= 0) & (xi == 0):
+            xf = x[1]
+    return 20/(1 + (20/x - 1)**2)
