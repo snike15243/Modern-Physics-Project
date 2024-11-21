02d4c010aa4e0946993075803ec103c5d8f55fa4
marnick1268 <M.A.Stoll@student.tudelft.nl>
2024-11-21 16:57:10 +0100

    l

--- Data/Good Luck Luka 2/B4/long/hy.py
+++ Data/Good Luck Luka 2/B4/long/hy.py
@@ -7,6 +7,8 @@ def ff(x,L,h,E):
     rho=3440
     return (x**2*h)/(4*np.pi*L**2)*np.sqrt(E/(3*rho))
 
+
+
 anlst=np.loadtxt(r'Resonance freq\an coeficient.txt',dtype=float)[:,1]*np.pi
 
 short=np.array([50.7,296])*1000
@@ -15,9 +17,19 @@ an=anlst[0:4]
 print(an)
 val, cov=curve_fit(ff,an,long,p0=[200*10**(-6),0.5*10**(-6),200*10**9])
 print(val)
+def ffshort(x,E):
+    rho=3440
+    L=100*10**(-6)
+    h=val[1]
+    return (x**2*h)/(4*np.pi*L**2)*np.sqrt(E/(3*rho))
 
-Lopt=np.linspace(180,200,400)
-hopt=np.linspace(0.4,0.6,400)*10**(-6)
+val2,cov2=curve_fit(ffshort,an[0:2],short,p0=[val[2]])
+fig=plt.figure()
+ax=fig.add_subplot()
+ax.plot(anlst[0:4],long)
+ax.plot(anlst[0:2],short)
+print(val2)
+plt.show()
 
 
 
